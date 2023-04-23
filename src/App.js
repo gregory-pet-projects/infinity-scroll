5cd083bc35cb94a295e64564d467c915dc7cdf25
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-23 20:33:57 +0300

    added table with styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const makeTableData = (w, h) => {
+  return new Array(h).fill(0).map((_, row) => {
+    return new Array(w).fill(0).map((_, col) => {
+      return row * 10 + col;
+    });
+  });
+};
+const ROW_HEIGHT = 40;
+const VISIBLE_ROWS = 3;
+
+const getTopHeight = () => {
+  return 0;
+};
+const getBottomHeight = () => {
+  return 0;
+};
+
+
+const App = () => {
+  const data = makeTableData(5, 10);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: ROW_HEIGHT * VISIBLE_ROWS + 1, overflow: "auto" }}>
+      <div style={{ height: getTopHeight() }}></div>
+
+      <table>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={`row-${rowIndex}`} style={{ height: ROW_HEIGHT }}>
+              {row.map((text, colIndex) => (
+                <td key={`col-${colIndex}`}>{text}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div style={{ height: getBottomHeight() }}></div>
     </div>
   );
-}
+};
 
 export default App;
